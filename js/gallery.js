4471ff702f74c0a2ec7abff8bb8f1fc12cf260df
Mr Stegosaurus <mrstego123@gmail.com>
2022-01-08 22:16:17 -0500

    preload images

--- js/gallery.js
+++ js/gallery.js
@@ -7,9 +7,14 @@ function activateGallery() {
     
 
     thumbnails.forEach(function(thumbnail) {
+        // Preload large images.
+        let newImageSrc = thumbnail.dataset.largeVersion;
+        let largeVersion = new Image();
+        largeVersion.nsrc = newImageSrc
+
+
         thumbnail.addEventListener('click', function() {
             // Set clicked image as main image.
-            let newImageSrc = thumbnail.dataset.largeVersion;
             mainImage.setAttribute("src", newImageSrc);
             mainImage.setAttribute("alt", thumbnail.alt);
 
